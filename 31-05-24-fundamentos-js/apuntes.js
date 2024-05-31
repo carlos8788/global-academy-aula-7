ba281db91e268e70eb1e3c67a487b85baa484b05
Luis <100473510+carlos8788@users.noreply.github.com>
2024-05-31 08:50:00 -0300

    fundamentos js

---
+++ 31-05-24-fundamentos-js/apuntes.js
@@ -0,0 +1,52 @@
+// Comentario de una linea
+
+/*
+Para 
+Colocar
+Acá lo que sea
+en la variable lista no me devuelve lo que quiero
+*/
+
+
+//** DATOS PRIMITIVOS **//
+
+// Cadena o Strings
+
+// ***************************************** //
+//                ENTRADA
+// ***************************************** //
+let espaniol = 'ESP'
+
+let ingles = "ENG"
+
+let portugues = `PORT`
+
+
+// ***************************************** //
+//                PROCESO
+// ***************************************** //
+espaniol.length // Me devuelve la longitud de la cadena
+
+espaniol[0] // 'E' => Me devuelve el elemento que tiene el índice dado
+
+espaniol.replace('E', 'A') // Remplaza las "E" por "A"
+
+
+// ***************************************** //
+//                SALIDA
+// ***************************************** //
+
+console.log(espaniol)
+console.log(espaniol.length)
+console.log(espaniol[0])
+console.log(espaniol.replace('S', 'A'))
+
+let inglesMinuscula = ingles.toLowerCase()
+
+console.log(ingles)
+console.log(inglesMinuscula)
+
+
+
+
+
